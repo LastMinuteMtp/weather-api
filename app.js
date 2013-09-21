a058eb44ae531f60cb663e78f75762e3c0456bd2
Thibaud Colas <thibaudcolas@gmail.com>
2013-09-21 10:22:57 +0200

    Finish v0.5 of the weather API

--- app.js
+++ app.js
@@ -1,43 +1,67 @@
 var express = require('express');
 var rest = require('restler');
+var condition = require('./model/condition');
 
 var app = express();
 
 var owm = {
   url: 'http://api.openweathermap.org/data/2.5/weather',
-  lang: '&lang=fr'
+  lang: '&lang=fr',
+  units: '&units=metric'
+};
+
+var critera = {
+  night: 'n',
+  tooCold : 10,
+  tooHot: 30,
+  tooWindy: 50
 };
 
 owm.get = function (parameters, callback) {
-  rest.get(owm.url + '?' + parameters + owm.lang).on('complete', callback);
+  rest.get(owm.url + '?' + parameters + owm.lang + owm.units, {parser: rest.parsers.json}).on('complete', callback);
 };
 
 app.get('/', function (req, resp) {
-  owm.get('q=Montpellier&lang=fr&mode=html', function (response) { resp.send(response); });
+  owm.get('q=Montpellier&lang=fr&mode=html', function (raw) { resp.send(raw); });
 });
 
 app.get('/weather/:lat/:long/raw.:format', function (req, resp) {
-  owm.get('lat=' + req.params.lat + '&lon=' + req.params.long + '&mode=' + req.params.format, function (response) { resp.send(response); });
+  owm.get('lat=' + req.params.lat + '&lon=' + req.params.long + '&mode=' + req.params.format, function (raw) { resp.send(raw); });
 });
 
 app.get('/weather/:place/raw.:format', function (req, resp) {
-  owm.get('q=' + req.params.place + '&mode=' + req.params.format, function (response) { resp.send(response); });
+  owm.get('q=' + req.params.place + '&mode=' + req.params.format, function (raw) { resp.send(raw); });
 });
 
 owm.process = function (raw) {
-  var ready = raw;
-  return ready;
+  return {
+    data: {
+      description : raw.weather[0].description,
+      icon: 'http://openweathermap.org/img/w/' + raw.weather[0].icon + '.png',
+      sunset: raw.sys.sunset,
+      temp: raw.main.temp,
+      wind: raw.wind.speed
+    },
+    analysis: {
+      isNight : raw.weather[0].icon[raw.weather[0].icon.length - 1] === critera.night,
+      isTooCold : raw.main.temp < critera.tooCold,
+      isTooHot : raw.main.temp > critera.tooHot,
+      isTooWindy : raw.wind.speed > critera.tooWindy
+    }
+  };
 };
 
-app.get('/weather/:lat/:long/ready.:format', function (req, resp) {
-  owm.get('lat=' + req.params.lat + '&lon=' + req.params.long + '&mode=' + req.params.format, function (response) {
-    resp.send(owm.process(response));
+app.get('/weather/:lat/:long/ready.json', function (req, resp) {
+  owm.get('lat=' + req.params.lat + '&lon=' + req.params.long + '&mode=json', function (raw) {
+    console.log(raw);
+    resp.send(owm.process(raw));
   });
 });
 
-app.get('/weather/:place/ready.:format', function (req, resp) {
-  owm.get('q=' + req.params.place + '&mode=' + req.params.format, function (response) {
-    resp.send(owm.process(response));
+app.get('/weather/:place/ready.json', function (req, resp) {
+  owm.get('q=' + req.params.place + '&mode=json', function (raw) {
+    console.log(raw);
+    resp.send(owm.process(raw));
   });
 });
 
